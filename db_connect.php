bdffdcc0221d34bbac6597c81ecf6f84e5802d3b
Derek Smith <smithda1@masters.edu>
2026-04-30 13:34:54 -0700

    credentials fix

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,17 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "blog_database";
+$servername = "146.190.144.31";
+$port = 33073;
+$username = "student_s013";
+$password = "5bIzdgSzmIV77J#KfMz-";
+$dbname = "blog_s013";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create connection 
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8mb4");
 ?>
